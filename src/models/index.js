0314dd19190d9324248cd7a7ba726d915269f0a5
galaxy4276 <galaxyhi4276@gmail.com>
2020-09-08 19:39:49 +0900

    feat: User 테이블 작성 및 적용

--- src/models/index.js
+++ src/models/index.js
@@ -1,23 +1,24 @@
 'use strict';
-import path from 'path';
 import Sequelize from 'sequelize';
 import { development } from './config';
+import User from './User';
 
 
 // const config = require(__dirname + '/../config/config.json')[env];
 const config = development;
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
-  
+export const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.User = User(sequelize, Sequelize);
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+// Object.keys(db).forEach(modelName => {
+//  if (db[modelName].associate) {
+//    db[modelName].associate(db);
+//  }
+// });
 
 export default db;  
-  
-  // Object.keys(db).forEach(modelName => {
-  //   if (db[modelName].associate) {
-  //     db[modelName].associate(db);
-  //   }
-  // });
