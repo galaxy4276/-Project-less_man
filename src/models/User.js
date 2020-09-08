c6518ec40f1eff40b43657d20b2113dd8e1ac18d
galaxy4276 <galaxyhi4276@gmail.com>
2020-09-08 19:42:05 +0900

    feat: User 모델 작성

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const User = (sequelize, DataTypes) => {
+  return sequelize.define('User', {
+    id: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      unique: 'username',
+    },
+    password: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+      unique: 'password',
+    },
+  }, {
+    timestamp: true,
+  });
+};
+
+
+export default User;
