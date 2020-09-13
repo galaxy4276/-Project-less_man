01a343f06a2ce69ebeb81cba3b4378cb63a96762
galaxy4276 <galaxyhi4276@gmail.com>
2020-09-13 23:44:54 +0900

    0913/feat/회원 가입 기능 구현 1단계 ( SQL INSERT )

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,4 +1,5 @@
 import sequelize from '../models';
+import bcrypt from 'bcrypt';
 
 
 const { User } = sequelize; 
@@ -8,11 +9,36 @@ export const localLogin = (req, res) => {
 };
 
 
-export const localJoin = (req, res) => {
+export const localJoin = async (req, res, next) => {
   const { id, passwd, verifypw } = req.body;
 
-  console.log(sequelize);
-  console.log(`id: ${id}\npasswd: ${passwd}\nverifypwd: ${verifypw}`);
+  try {
+    const user = await User.findByPk(id);
+
+    const pw = await bcrypt.hash(passwd, 10);
+    console.log(`created pw: ${pw}`);
+
+    if (user) {
+      console.log('유저가 이미 존재합니다.');
+      next();
+    }
+
+    if ( !user && (passwd === verifypw)) {
+      await User.create({
+        id,
+        name: 'unknown1',
+        password: await (await bcrypt.hash(passwd, 10)).slice(0, 30),
+      });
+      console.log('계정이 생성 되었습니다.');
+    } else {
+      console.log('계정 생성 실패');
+      res.redirect('/join');
+    }
+  } catch (e) {
+    console.error(e);
+    console.log('에러가 발생하였습니다.');
+    res.redirect('/');
+  }
 
   res.redirect('/');
 };
