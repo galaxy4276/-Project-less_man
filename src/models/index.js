a6171deb63919e42870eac7ba44f447e429e9b43
galaxy4276 <galaxyhi4276@gmail.com>
2020-09-26 16:10:43 +0900

    feat: 사용자와 게시글 간 참조관계 선언

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@
 import Sequelize from 'sequelize';
 import { development } from './config';
 import User from './User';
+import Post from './Post';
 
 
 // const config = require(__dirname + '/../config/config.json')[env];
@@ -11,10 +12,14 @@ const db = {};
 export const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.User = User(sequelize, Sequelize);
+db.Post = Post(sequelize, Sequelize);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+
+db.User.hasMany(db.Post);
+db.Post.belongsTo(db.User);
 // Object.keys(db).forEach(modelName => {
 //  if (db[modelName].associate) {
 //    db[modelName].associate(db);
