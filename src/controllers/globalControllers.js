dbff91d81dad1b8100d1df685ed0a077889286ee
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-03 15:54:14 +0900

    feat: 게시글 전체 로딩 유저정보도 로딩

--- src/controllers/globalControllers.js
+++ src/controllers/globalControllers.js
@@ -1,10 +1,14 @@
 import sequelize from '../models';
-
-const { Post } = sequelize;
+const { Post, User } = sequelize;
 
 
 export const homeLoading = async (req, res) => {
-  const articleList = await Post.findAll({});
+  const articleList = await Post.findAll({
+    include: [{
+      model: User,
+      attributes: ['userid']
+    }],
+  });
 
   console.log('Cookies: ', JSON.stringify(req.cookies, null, 2));
   console.log('Signed Cookies: ', JSON.stringify(req.signedCookies, null, 2));
