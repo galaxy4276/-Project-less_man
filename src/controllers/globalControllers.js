a24dffe6ed27302225fe6ce9692443c6ba78df7c
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-08 09:04:26 +0900

    feat: homeLoading Image 포함

--- src/controllers/globalControllers.js
+++ src/controllers/globalControllers.js
@@ -1,5 +1,5 @@
 import sequelize from '../models';
-const { Post, User } = sequelize;
+const { Post, User, Image } = sequelize;
 
 
 export const homeLoading = async (req, res) => {
@@ -7,8 +7,13 @@ export const homeLoading = async (req, res) => {
     include: [{
       model: User,
       attributes: ['userid']
+    }, {
+      model: Image,
+      attributes: ['src'],
+      limit: 1,
     }],
   });
+  console.log(articleList);
 
   console.log('Cookies: ', JSON.stringify(req.cookies, null, 2));
   console.log('Signed Cookies: ', JSON.stringify(req.signedCookies, null, 2));
